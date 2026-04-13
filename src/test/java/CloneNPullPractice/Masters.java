9b00bee93b671b54780b0ad6a5e64d5652384c61
HP <HP@DESKTOP-5B0GRDA>
2026-04-13 12:54:13 +0530

    from FE to understand git conflict

--- src/test/java/CloneNPullPractice/Masters.java
+++ src/test/java/CloneNPullPractice/Masters.java
@@ -3,6 +3,7 @@ package CloneNPullPractice;
 public class Masters {
 
 	public static void main(String[] args) {
-		System.out.println("Selecet new masters");
+		System.out.println("Select new masters");
+		System.out.println("From FE to understand git conflict");
 	}
 }
